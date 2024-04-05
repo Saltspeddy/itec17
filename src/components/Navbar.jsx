a36d7fbb23f9d20927dfc1d73dea3db8b9945b45
Saltspeddy <walkercoco30@gmail.com>
2024-04-05 19:38:50 +0300

    routes created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 function Navbar() {
   return (
     <div className="bg-[#414361] text-xl font-roboto flex flex-col justify-center items-start">
       <div>
-        <ul>
-          <li className="text-xl">Home</li>
-          <li className="text-xl">Dashboard</li>
-          <li></li>
+        <ul className="text-xl">
+          <Link to="/"> Home</Link>
+          <Link to="/dashboard" className="text-xl">
+            Dashboard
+          </Link>
+          <Link to="login">login</Link>
         </ul>
       </div>
     </div>
